b2f51b28b71dd61581ca4d49e19b404b99d744f1
Martin Coplák <martin.coplak@gmail.com>
2021-01-13 22:42:23 +0100

    Updated headers

--- src/main/kotlin/eu/warfaremc/prestige/listener/GUI.kt
+++ src/main/kotlin/eu/warfaremc/prestige/listener/GUI.kt
@@ -1,7 +1,7 @@
 /*
- * This file is part of Millennium, licensed under the MIT License.
+ * This file is part of WarfareMC, licensed under the MIT License.
  *
- * Copyright (C) 2020 Millennium & Team
+ * Copyright (C) 2020 WarfareMC & Team
  *
  * Permission is hereby granted, free of charge,
  * to any person obtaining a copy of this software and associated documentation files (the "Software"),
